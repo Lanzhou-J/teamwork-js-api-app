ce278b9274e7c1c2b1ae1d6d22218fc97086b58d
Lanzhou Jiang <lzjiang99@gmail.com>
2020-06-04 23:22:35 +1000

    add readline

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const fetch = require("node-fetch")
+const readline = require("readline-sync")
 const parseString = require("xml2js").parseString;
+const clear = require('clear');
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+clear();
+console.log(
+  chalk.cyan(
+    figlet.textSync('Readapi', { horizontalLayout: 'full' })
+  )
+);
+
+console.log("Hi, which author do you want to check?")
+console.log("Please select from the options:")
+console.log("JK Rowling")
+console.log("Charles Dickens")
+console.log("Stefan Zweig")
+const author = readline.question("> ")
 
 const getAuthor = () => {
 return fetch("https://www.goodreads.com/author/show.xml?key=cfyfMOfygbaO23YVVBiA&id=1077326.J_K_Rowling").then(response => response.text()).then(data => data) 
@@ -11,7 +29,7 @@ promises.push(getAuthor())
 Promise.all(promises).then((response)=> {
   let xml=response[0]
   parseString(xml, function (err, result) {
-    console.dir(result["GoodreadsResponse"]["author"][0]["books"][0]["book"][0]);
+    console.dir(result["GoodreadsResponse"]["author"][0].name);
   });
 })
 
